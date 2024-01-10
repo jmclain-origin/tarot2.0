2d3f273d87962c3ffa3f89011b0444eddf8e83e4
jmclain <jmclain3@asu.edu>
2024-01-10 12:26:53 -0700

    feat(navbar): created Navbar component with simple ui
    created new file, added supporting modules. icons, fonts, etc

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,17 +2,18 @@
 import type { NavLink } from "../types/Navbar";
 const isAuth = ref(false);
 const links: NavLink[] = [
-  { title: "Home", path: "/", icon: "" },
-  { title: "About", path: "/about", icon: "" },
+  { title: "Home", path: "/", icon: "i-mdi:home" },
+  { title: "About", path: "/about", icon: "i-mdi:information" },
 ];
+const linkStyleTailwind = "text-2xl text-slate-900 hover:text-slate-700 hover:text-slate-600";
 </script>
 <template>
   <div class="w-[100vw] h-[50px] bg-slate-200">
     <div class="flex row justify-center items-center w-[100%] space-x-4 w-100">
-      <NuxtLink v-for="link in links" :to="link.path"
-        >{{ link.title }}<i :class="link.icon"></i
-      ></NuxtLink>
-      <NuxtLink v-if="!isAuth" to="/login">Login</NuxtLink>
+      <NuxtLink v-for="link in links" :to="link.path" :class="linkStyleTailwind"
+        >{{ link.title }}<Icon :name="link.icon"/>
+      </NuxtLink>
+      <NuxtLink v-if="!isAuth" to="/login" :class="linkStyleTailwind">Login<Icon name="i-mdi:login"></Icon></NuxtLink>
     </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@formkit/auto-animate'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@formkit/auto-animate', '@nuxtjs/google-fonts', 'nuxt-icon-tw', '@nuxtjs/tailwindcss'],
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
     config:{
@@ -11,5 +11,8 @@ export default defineNuxtConfig({
   },
   image: {
     inject: true,
-  }
+  },
+  // googleFonts: {
+  //   angerine: true,
+  // }
 })
